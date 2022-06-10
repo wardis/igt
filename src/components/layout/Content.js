2bb3c4126dd3f3a39f2f70421a60d3b328f26ee9
Wardi <wardi@wardi-s.com>
2022-06-10 04:48:02 +0100

    spacing between section

--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -3,14 +3,19 @@ import { Hero } from './Hero';
 import { Features } from './Features';
 import { Blog } from './Blog';
 import { Newsletter } from './Newsletter';
+import { Stack, Container } from '@mui/material';
 
 export const Content = () => {
   return (
     <main>
-      <Hero />
-      <Features />
-      <Blog />
-      <Newsletter />
+      <Container sx={{ my: 3 }}>
+        <Stack spacing={3} justifyContent='space-around'>
+          <Hero />
+          <Features />
+          <Blog />
+          <Newsletter />
+        </Stack>
+      </Container>
     </main>
   );
 };
