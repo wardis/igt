4066676cce5bb0cd4d53a08d5e1b7f2eb9e3e798
Wardi <wardi@wardi-s.com>
2022-06-10 10:03:05 +0100

    newsletter

--- src/components/layout/Newsletter.js
+++ src/components/layout/Newsletter.js
@@ -1,3 +1,28 @@
+import { Box, Button, Stack, Typography, TextField } from '@mui/material';
 export const Newsletter = () => {
-  return <section>Newsletter Section</section>;
+  return (
+    <section>
+      <Stack direction='row' spacing={2}>
+        <Typography>Abonnez-vous à notre newsletter</Typography>
+        <TextField
+          label='Nom'
+          size='small'
+          sx={{ backgroundColor: '#f1f1f1' }}
+        />
+        <TextField
+          label='Adresse e-mail'
+          size='small'
+          type='email'
+          sx={{ backgroundColor: '#f1f1f1' }}
+        />
+        <Button variant='contained'>S'abonner</Button>
+      </Stack>
+      <Box borderBottom={10} py={3}>
+        <Typography maxWidth={600}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet ex
+          adipisci temporibus impedit aut dignissimos ratione nobis ut nostrum
+        </Typography>
+      </Box>
+    </section>
+  );
 };
