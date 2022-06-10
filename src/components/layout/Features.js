71642dfb7d3d637cd8c95e80314e01b874010a73
Wardi <wardi@wardi-s.com>
2022-06-10 05:49:55 +0100

    features section

--- src/components/layout/Features.js
+++ src/components/layout/Features.js
@@ -1,14 +1,51 @@
-import { Box, Grid, Card, Stack, CardMedia, Typography } from '@mui/material';
+import {
+  Button,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@mui/material';
 
 export const Features = () => {
   return (
     <section>
-      <Grid container spacing={4}>
+      <Grid container spacing={4} sx={{ textAlign: 'center' }}>
         <Grid item xs={12} md={6}>
-          <Card>Hello</Card>
+          <Card>
+            <CardContent>
+              <Typography variant='h5' component='div'>
+                Lorem Ipsum Lorem
+              </Typography>
+              <Typography variant='body1'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
+                natus quo odit quia ea debitis, rem harum iure a quos sunt
+                vitae? Similique maiores, officia nostrum optio ab eaque
+                accusamus!
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'center' }}>
+              <Button variant='contained'>Lorem Ipsum</Button>
+            </CardActions>
+          </Card>
         </Grid>
         <Grid item xs={12} md={6}>
-          <Card>Hello 3</Card>
+          <Card>
+            <CardContent>
+              <Typography variant='h5' component='div'>
+                Lorem Ipsum Lorem
+              </Typography>
+              <Typography variant='body1'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam
+                natus quo odit quia ea debitis, rem harum iure a quos sunt
+                vitae? Similique maiores, officia nostrum optio ab eaque
+                accusamus!
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'center' }}>
+              <Button variant='contained'>Lorem Ipsum</Button>
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
     </section>
--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -44,7 +44,7 @@ export const Hero = () => {
   return (
     <Box bgcolor='gray'>
       <section>
-        <Grid container my={3} spacing={2}>
+        <Grid container my={4} spacing={3}>
           <Grid item xs={12} md={8}>
             <Carousel>
               {items.map((item, i) => (
